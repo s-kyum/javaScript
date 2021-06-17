2cd0ff7ba44844f83c42dd7111662511c2d7a2c4
s-kyum@naver.com <s-kyum@naver.com>
2021-06-17 10:04:43 +0900

    자바 추가파일

--- js/3.showhide.js
+++ js/3.showhide.js
@@ -2,10 +2,15 @@
 function showText(){
     var text = document.getElementById("desc");
     text.style.display = "block"; //수직으로 보이기
-}
+    var button = document.getElementById("btn");
+    button.style.display = "none";}
+
+    // document.getElementById("desc").style.display ="block";   * 변수설정없이 한번에 하는 법
+    // document.getElementById("btn").style.display ="none";
 
 //상세 설명 닫기 클릭-텍스트 숨기기
 function hideText(){
     var text = document.getElementById("desc");
     text.style.display = "none"; //수직으로 보이기
-}
+    var button = document.getElementById("btn");
+    button.style.display = "block";}
